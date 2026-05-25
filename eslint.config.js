3c298954ca3c267fc98aae770e6b21565738fbcd
cheliv88 <mignaquiemiliano@gmail.coml>
2026-05-25 19:21:08 -0300

    rules off

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import sonarjs from 'eslint-plugin-sonarjs' // <-- 1. Importamos SonarJS
+import sonarjs from 'eslint-plugin-sonarjs'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -26,6 +26,13 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      
+      'sonarjs/no-ignored-exceptions': 'off',
+      'sonarjs/cognitive-complexity': 'off',
+      'sonarjs/no-nested-conditional': 'off',
+      'sonarjs/unused-import': 'warn',
+
+      'react-hooks/static-components': 'off', 
     },
   },
 ])
